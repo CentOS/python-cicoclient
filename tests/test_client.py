b0db90ce0ac95389ce8e1371de35eab2c58f2fac
Evgeni Golov <evgeni@golov.de>
2021-10-22 10:42:15 +0200

    add tests for error conditions

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,3 +10,15 @@ def test_get(requests_mock):
     requests_mock.get('http://api.example.com/', request_headers=expected_headers, json={'status': 'OK'})
     resp, body = cico.get('/')
     assert body == {'status': 'OK'}
+
+def test_get_string_body(requests_mock):
+    cico = client.CicoClient(endpoint='http://api.example.com')
+    requests_mock.get('http://api.example.com/', json="Failed to allocate nodes")
+    resp, body = cico.get('/')
+    assert body == "Failed to allocate nodes"
+
+def test_get_bad_body(requests_mock):
+    cico = client.CicoClient(endpoint='http://api.example.com')
+    requests_mock.get('http://api.example.com/', text="Invalid JSON")
+    resp, body = cico.get('/')
+    assert body is None
--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -1,4 +1,4 @@
-from cicoclient import wrapper
+from cicoclient import wrapper, exceptions
 
 import pytest
 
@@ -63,6 +63,24 @@ def test_node_get(cicowrapper, requests_mock, inventory_mock):
     assert 'n1.hufty' in inventory
     assert inventory['n1.hufty']['ip_address'] == "172.19.3.1"
 
+def test_node_get_no_body(cicowrapper, requests_mock, inventory_mock):
+    requests_mock.get('http://api.example.com/Node/get?key=dummy_key&arch=x86_64')
+    with pytest.raises(exceptions.NoInventory) as excinfo:
+        cicowrapper.node_get(arch='x86_64', retry_interval=1)
+    assert str(excinfo.value) == "The requested operation failed as no inventory is available."
+
+def test_node_get_error(cicowrapper, requests_mock, inventory_mock):
+    requests_mock.get('http://api.example.com/Node/get?key=dummy_key&arch=x86_64', json="Failed to allocate nodes")
+    with pytest.raises(ValueError) as excinfo:
+        cicowrapper.node_get(arch='x86_64')
+    assert str(excinfo.value) == "Failed to allocate nodes"
+
 def test_node_done(cicowrapper, requests_mock, inventory_mock):
     requests_mock.get('http://api.example.com/Node/done?key=dummy_key&ssid=deadtest')
     cicowrapper.node_done(ssid='deadtest')
+
+def test_node_done_without_ssid(cicowrapper, requests_mock, inventory_mock):
+    requests_mock.get('http://api.example.com/Node/done?key=dummy_key')
+    with pytest.raises(exceptions.SsidRequired) as excinfo:
+        cicowrapper.node_done()
+    assert str(excinfo.value) == "The requested operation requires a SSID."
