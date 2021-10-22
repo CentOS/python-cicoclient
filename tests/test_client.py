7b4fbfc3f1a4ddeeffc44d52c2657967b58c54b0
Evgeni Golov <evgeni@golov.de>
2021-10-22 10:44:58 +0200

    move client definition to a fixture

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,24 +1,27 @@
 from cicoclient import client
 
-def test_get(requests_mock):
+import pytest
+
+@pytest.fixture
+def cico():
+    return client.CicoClient(endpoint='http://api.example.com')
+
+def test_get(requests_mock, cico):
     expected_headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'User-Agent': 'python-cicoclient',
     }
-    cico = client.CicoClient(endpoint='http://api.example.com')
     requests_mock.get('http://api.example.com/', request_headers=expected_headers, json={'status': 'OK'})
     resp, body = cico.get('/')
     assert body == {'status': 'OK'}
 
-def test_get_string_body(requests_mock):
-    cico = client.CicoClient(endpoint='http://api.example.com')
+def test_get_string_body(requests_mock, cico):
     requests_mock.get('http://api.example.com/', json="Failed to allocate nodes")
     resp, body = cico.get('/')
     assert body == "Failed to allocate nodes"
 
-def test_get_bad_body(requests_mock):
-    cico = client.CicoClient(endpoint='http://api.example.com')
+def test_get_bad_body(requests_mock, cico):
     requests_mock.get('http://api.example.com/', text="Invalid JSON")
     resp, body = cico.get('/')
     assert body is None
